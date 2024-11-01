5cfaf9ce061baabca97348eb2b03874523f2913a
Ilonczai András <ilonczai.andras16@gmail.com>
2024-11-01 19:02:29 +0100

    rename lab6 and format files

---
+++ lab6/1_A_Blinking_Rate.py
@@ -0,0 +1,195 @@
+from sense_hat import SenseHat
+import time
+
+delay_val = 1.0
+
+
+def delay(event):
+    global delay_val
+    if event.action == "pressed":
+
+        if event.direction == "middle":
+
+            if event.action == "pressed":
+                delay_val = 0.2
+            elif event.action == "released":
+                delay_val = 1.0
+
+        elif event.direction == "up":
+
+            if event.action == "pressed":
+                delay_val = 0.4
+            elif event.action == "released":
+                delay_val = 2.0
+
+        elif event.direction == "down":
+
+            if event.action == "pressed":
+                delay_val = 0.8
+            elif event.action == "released":
+                delay_val = 4.0
+
+        elif event.direction == "left":
+
+            if event.action == "pressed":
+                delay_val = 0.16
+            elif event.action == "released":
+                delay_val = 8.0
+
+        else:
+            if event.action == "pressed":
+                delay_val = 0.32
+            elif event.action == "released":
+                delay_val = 16.0
+
+
+sense = SenseHat()
+
+sense.stick.direction_middle = delay
+sense.stick.direction_up = delay
+sense.stick.direction_down = delay
+sense.stick.direction_right = delay
+sense.stick.direction_left = delay
+
+w = (255, 255, 255)
+n = (0, 0, 0)
+
+on = [
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+    w,
+]
+
+off = [
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+    n,
+]
+
+while True:
+    sense.set_pixels(on)
+    time.sleep(delay_val)
+    sense.set_pixels(off)
+    time.sleep(delay_val)
--- lab6/4_Rain_Effect.py
+++ lab6/4_Rain_Effect.py
@@ -10,20 +10,19 @@ raindrop_color = (0, 0, 255)
 sense.clear()
 
 while True:
-    
+
     sor = random.randint(0, 7)
-    
+
     sense.set_pixel(sor, 0, raindrop_color)
-    
+
     time.sleep(0.5)
-    
-    for sor in range(7, 0 , -1):
+
+    for sor in range(7, 0, -1):
 
         for oszlop in range(0, 8, 1):
 
             pixel = sense.get_pixel(oszlop, sor - 1)
             sense.set_pixel(oszlop, sor, pixel)
 
-    
     for sor in range(8):
         sense.set_pixel(sor, 0, (0, 0, 0))
