da02bd704459ea5120d6f70eeb0e4aae2a238415
Ilonczai András <ilonczai.andras16@gmail.com>
2024-11-01 19:05:59 +0100

    rename lab8 and format files

--- lab8/8_Pi_Cam_Video.py
+++ lab8/8_Pi_Cam_Video.py
@@ -6,7 +6,13 @@ import time
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--picamera", type=int, default=1, help="Pi camera or the USB camera will be used")
+ap.add_argument(
+    "-p",
+    "--picamera",
+    type=int,
+    default=1,
+    help="Pi camera or the USB camera will be used",
+)
 args = vars(ap.parse_args())
 
 vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
@@ -15,17 +21,24 @@ time.sleep(2.0)
 while True:
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
-    
+
     timestamp = datetime.datetime.now()
     ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-    cv2.putText(frame, ts, (10, frame.shape[0] - 10),
-    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    
+    cv2.putText(
+        frame,
+        ts,
+        (10, frame.shape[0] - 10),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.35,
+        (0, 0, 255),
+        1,
+    )
+
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
-    
+
     if key == ord("q"):
         break
-    
+
 cv2.destroyAllWindows()
 vs.stop()
