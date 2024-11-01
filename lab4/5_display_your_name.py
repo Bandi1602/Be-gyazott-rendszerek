4e3ec2fa15c6ae2e21ddfa49214c37b7fdc04e61
Ilonczai András <ilonczai.andras16@gmail.com>
2024-11-01 19:00:08 +0100

    rename lab4 and format files

--- lab4/5_display_your_name.py
+++ lab4/5_display_your_name.py
@@ -2,12 +2,14 @@ from sense_hat import SenseHat
 from random import randint
 import time
 
+
 def random_colour():
-# randint - random integer between an interval
+    # randint - random integer between an interval
     random_red = randint(0, 255)
     random_green = randint(0, 255)
     random_blue = randint(0, 255)
     return (random_red, random_green, random_blue)
+
 
 sense = SenseHat()
 
